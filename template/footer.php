787d185f1c9941605b08ccf59d342703a307b07b
LaibaKhan@19 <165555528+lsk-786@users.noreply.github.com>
2025-07-17 23:22:02 +0530

    Add files via upload

---
+++ template/footer.php
@@ -0,0 +1,18 @@
+      	<hr>
+
+      	<footer>
+        	<div class="text-muted pull-left">
+            	<a href="" target="_blank"> Laiba Khan </a>
+        	</div>
+        	<div class="text-muted pull-right">
+          		<a href="admin.php">Admin Login</a>
+        	</div>
+      	</footer>
+    </div> <!-- /container -->
+
+
+    
+    <script type="text/javascript" src="./bootstrap/js/jquery-2.1.4.min.js"></script>
+    <script type="text/javascript" src="./bootstrap/js/bootstrap.min.js"></script>
+  </body>
+</html>
